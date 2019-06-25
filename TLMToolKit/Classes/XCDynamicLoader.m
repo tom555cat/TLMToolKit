18971e47ae1adc165897654590c20b5c89d4ac1f
Tong Leiming <tongleiming@daling.com>
2019-06-25 16:30:47 +0800

    修改宏定义。

--- TLMToolKit/Classes/XCDynamicLoader.m
+++ TLMToolKit/Classes/XCDynamicLoader.m
@@ -12,6 +12,45 @@
 #include <dlfcn.h>
 #import "XCModuleManager.h"
 
+#ifdef __LP64__
+typedef uint64_t XCExportValue;
+typedef struct section_64 XCExportSection;
+#define XCGetSectByNameFromHeader getsectbynamefromheader_64
+#else
+typedef uint32_t XCExportValue;
+typedef struct section XCExportSection;
+#define XCGetSectByNameFromHeader getsectbynamefromheader
+#endif
+
+//NSArray<NSValue *>* XCReadModuleInitFunction(char *sectionName, const struct mach_header *mhp);
+
+//static void dyld_callback(const struct mach_header *mhp, intptr_t vmaddr_slide) {
+//    for (NSString *level in [XCModuleManager sharedManager].levelArray) {
+//        char *sectionName = (char *)[level cStringUsingEncoding:NSUTF8StringEncoding];
+//        if (sectionName) {
+//            NSArray *funcArray = XCReadModuleInitFunction(sectionName, mhp);
+//            [[XCModuleManager sharedManager] addModuleInitFuncs:funcArray forLevel:level];
+//        }
+//    }
+//}
+
+//NSArray<NSValue *>* XCReadModuleInitFunction(char *sectionName, const struct mach_header *mhp) {
+//    NSMutableArray *funcArray = [NSMutableArray array];
+//    unsigned long size = 0;
+//#ifndef __LP64__
+//    uintptr_t *memory = (uintptr_t*)getsectiondata(mhp, SEG_DATA, sectionName, &size);
+//#else
+//    const struct mach_header_64 *mhp64 = (const struct mach_header_64 *)mhp;
+//    uintptr_t *memory = (uintptr_t*)getsectiondata(mhp64, SEG_DATA, sectionName, &size);
+//#endif
+//
+//    unsigned long counter = size/sizeof(void*);
+//    for (int idx = 0; idx < counter; ++idx) {
+//        XCDynamicLoaderInjectFunction func = (XCDynamicLoaderInjectFunction)memory[idx];
+//        [funcArray addObject:[NSValue valueWithPointer:func]];
+//    }
+//    return funcArray;
+//}
 
 static void XCDynamicLoader_invoke_method(NSString *level){
     NSArray *funcArray = [[[XCModuleManager sharedManager] modInitFuncPtrArrayLevelDic] objectForKey:level];
@@ -19,55 +58,35 @@ static void XCDynamicLoader_invoke_method(NSString *level){
         XCDynamicLoaderInjectFunction func = val.pointerValue;
         func();
     }
-    
-//    Dl_info info;
-//    int ret = dladdr(XCDynamicLoader_invoke_method, &info);
-//    if(ret == 0){
-//        // fatal error
-//    }
-//#ifndef __LP64__
-//    const struct mach_header *mhp = (struct mach_header*)info.dli_fbase;
-//    unsigned long size = 0;
-//    uint32_t *memory = (uint32_t*)getsectiondata(mhp, QWLoadableSegmentName, QWLoadableSectionName, & size);
-//#else /* defined(__LP64__) */
-//    const struct mach_header_64 *mhp = (struct mach_header_64*)info.dli_fbase;
-//    unsigned long size = 0;
-//    uint64_t *memory = (uint64_t*)getsectiondata(mhp, XCDYML_SEGMENTNAME, key, &size);
-//#endif /* defined(__LP64__) */
-//
-//    for(int idx = 0; idx < size/sizeof(void*); ++idx){
-//        XCDynamicLoaderInjectFunction func = (XCDynamicLoaderInjectFunction)memory[idx];
-//        func(); //crash tofix
-//    }
 }
 
-NSArray<NSValue *>* XCReadModuleInitFunction(char *sectionName, const struct mach_header *mhp);
+
+NSArray<NSValue *>* XCReadSection(char *sectionName, const struct mach_header *mhp);
+
 static void dyld_callback(const struct mach_header *mhp, intptr_t vmaddr_slide) {
     for (NSString *level in [XCModuleManager sharedManager].levelArray) {
-        char *sectionName = (char *)[level cStringUsingEncoding:NSUTF8StringEncoding];
-        if (sectionName) {
-            NSArray *funcArray = XCReadModuleInitFunction(sectionName, mhp);
-            [[XCModuleManager sharedManager] addModuleInitFuncs:funcArray forLevel:level];
-        }
+        NSString *fKey = [NSString stringWithFormat:@"__%@%s", level?:@"", XCDYML_SECTION_SUFFIX];
+        NSArray *funcArray = XCReadSection((char *)[fKey UTF8String], mhp);
+        [[XCModuleManager sharedManager] addModuleInitFuncs:funcArray forLevel:level];
     }
 }
 
-NSArray<NSValue *>* XCReadModuleInitFunction(char *sectionName, const struct mach_header *mhp) {
+NSArray<NSValue *>* XCReadSection(char *sectionName, const struct mach_header *mhp) {
     NSMutableArray *funcArray = [NSMutableArray array];
-    unsigned long size = 0;
-#ifndef __LP64__
-    uintptr_t *memory = (uintptr_t*)getsectiondata(mhp, SEG_DATA, sectionName, &size);
-#else
-    const struct mach_header_64 *mhp64 = (const struct mach_header_64 *)mhp;
-    uintptr_t *memory = (uintptr_t*)getsectiondata(mhp64, SEG_DATA, sectionName, &size);
-#endif
     
-    unsigned long counter = size/sizeof(void*);
-    for (int idx = 0; idx < counter; ++idx) {
-        XCDynamicLoaderInjectFunction func = (XCDynamicLoaderInjectFunction)memory[idx];
-        [funcArray addObject:[NSValue valueWithPointer:func]];
-//        [[XCModuleManager sharedManager] addModuleInitFunc:func forLevel:[NSString stringWithUTF8String:sectionName]];
+    const XCExportValue mach_header = (XCExportValue)mhp;
+    const XCExportSection *section = XCGetSectByNameFromHeader((void *)mach_header, XCDYML_SEGMENTNAME, sectionName);
+    if (section == NULL) return @[];
+    
+    int addrOffset = sizeof(struct XC_Function);
+    for (XCExportValue addr = section->offset;
+         addr < section->offset + section->size;
+         addr += addrOffset) {
+        
+        struct XC_Function entry = *(struct XC_Function *)(mach_header + addr);
+        [funcArray addObject:[NSValue valueWithPointer:entry.function]];
     }
+    
     return funcArray;
 }
 
@@ -78,8 +97,12 @@ void initXCProphet() {
 
 @implementation XCDynamicLoader
 
-+ (void)executeFunctionsForLevel:(NSString *)level {
-    XCDynamicLoader_invoke_method(level);
+//+ (void)executeFunctionsForLevel:(NSString *)level {
+//    XCDynamicLoader_invoke_method(level);
+//}
+
++ (void)executeFunctionsForKey:(NSString *)key {
+    XCDynamicLoader_invoke_method(key);
 }
 
 @end
